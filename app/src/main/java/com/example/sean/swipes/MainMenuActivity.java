5d63f90843b348ca1a7ad9908f1cd50444256b46
SeanStahly <seanstahly@gmail.com>
2016-05-30 13:10:39 -0500

    Added basic views, with little to no functionality so far

--- app/src/main/java/com/example/sean/swipes/MainMenuActivity.java
+++ app/src/main/java/com/example/sean/swipes/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.sean.swipes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    /**called when the user hits the "play" button */
+    public void playGame(View view) {
+        Intent intent = new Intent(this, StandardGame.class);
+        startActivity(intent);
+    }
+
+    public void viewHighScores(View view) {
+        Intent intent = new Intent(this, HighScores.class);
+        startActivity(intent);
+    }
+
+    public void viewSettings(View view) {
+        Intent intent = new Intent(this, SettingsMenu.class);
+        startActivity(intent);
     }
 }
